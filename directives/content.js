7da6d65e8b1ff1896a2ee3ce8b46d4f458ea202b
Alexander Verbruggen <alex@nabu.be>
2018-05-03 17:49:59 +0200

    u

--- directives/content.js
+++ directives/content.js
@@ -10,6 +10,11 @@ Vue.directive("content", {
 		if (keys && keys.indexOf("sanitize") >= 0) {
 			content = nabu.utils.elements.sanitize(content);
 		}
+		// we interpret this as plain string data, that means making sure everything is escaped and whitespace is adhered to
+		if (keys && keys.indexOf("plain") >= 0) {
+			content = content.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
+				.replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
+		}
 		if (keys && keys.indexOf("compile") >= 0) {
 			var component = Vue.extend({
 				data: function() {
