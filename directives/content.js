6a904a6c4069dd0489c8150db561ad1662049805
Alexander Verbruggen <alex@nabu.be>
2018-05-08 17:26:42 +0200

    updated

--- directives/content.js
+++ directives/content.js
@@ -1,19 +1,22 @@
-Vue.directive("content", {
-	// the argument should be the name of the route, any value is passed in as parameters
-	// the modifier is interpreted as the anchor to route it to
-	bind: function(element, binding, vnode) {
-		var content = binding.value;
-		var keys = null;
-		if (binding.modifiers) {
-			keys = Object.keys(binding.modifiers);
-		}
+// the argument should be the name of the route, any value is passed in as parameters
+// the modifier is interpreted as the anchor to route it to
+Vue.directive("content", function(element, binding, vnode) {
+	var content = binding.value;
+	var keys = null;
+	if (binding.modifiers) {
+		keys = Object.keys(binding.modifiers);
+	}
+	// always clear the element
+	nabu.utils.elements.clear(element);
+	if (content) {
 		if (keys && keys.indexOf("sanitize") >= 0) {
 			content = nabu.utils.elements.sanitize(content);
 		}
 		// we interpret this as plain string data, that means making sure everything is escaped and whitespace is adhered to
 		if (keys && keys.indexOf("plain") >= 0) {
 			content = content.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
-				.replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;");
+				.replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")
+				.replace(/ /g, "&nbsp;");
 		}
 		if (keys && keys.indexOf("compile") >= 0) {
 			var component = Vue.extend({
