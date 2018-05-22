01450ee47df7f8ee917ea5b12181bd2bfd72e4fc
Alexander Verbruggen <alex@nabu.be>
2018-05-22 09:45:28 +0200

    updated

--- directives/content.js
+++ directives/content.js
@@ -9,49 +9,46 @@ Vue.directive("content", function(element, binding, vnode) {
 	// always clear the element
 	nabu.utils.elements.clear(element);
 	if (content != null && typeof(content) != "undefined") {
-		if (keys && keys.indexOf("sanitize") >= 0) {
-			content = nabu.utils.elements.sanitize(content);
+		var parameters = {};
+		if (keys && keys.indexOf("parameterized") >= 0) {
+			parameters = content;
+			content = content.value;
 		}
-		// we interpret this as plain string data, that means making sure everything is escaped and whitespace is adhered to
-		if (keys && keys.indexOf("plain") >= 0) {
-			content = content.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
-				.replace(/[\r\n]+/g, "<br/>").replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")
-				.replace(/ /g, "&nbsp;");
-		}
-		if (keys && keys.indexOf("compile") >= 0) {
-			var context = {};
-			Object.keys(vnode.context.$props).map(function(key) {
-				context[key] = vnode.context.$props[key];
-			});
-			// data overwrites props if necessary
-			Object.keys(vnode.context.$data).map(function(key) {
-				context[key] = vnode.context.$data[key];
-			});
-			var component = Vue.extend({
-				data: function() {
-					return context;
-				},
-				template: "<div>" + (typeof(content) == "string" ? content : content.innerHTML) + "</div>" 
-			});
-			content = new component();
-			content.$mount();
-			var lastInserted = null;
-			// we added a wrapper div, strip it again
-			for (var i = content.$el.childNodes.length - 1; i >= 0; i--) {
-				if (!lastInserted) {
-					lastInserted = element.appendChild(content.$el.childNodes[i]);
-				}
-				else {
-					lastInserted = element.insertBefore(content.$el.childNodes[i], lastInserted);
-				}
+		if (content != null && typeof(content) != "undefined") {
+			if ((keys && keys.indexOf("sanitize") >= 0) || parameters.sanitize) {
+				content = nabu.utils.elements.sanitize(content);
 			}
-			//element.appendChild(content.$el);
-		}
-		else if (typeof(content) == "string") {
-			element.innerHTML = content;
-		}
-		else {
-			element.appendChild(content);
+			// we interpret this as plain string data, that means making sure everything is escaped and whitespace is adhered to
+			if ((keys && keys.indexOf("plain") >= 0) || parameters.plain) {
+				content = content.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;")
+					.replace(/\n/g, "<br/>").replace(/\t/g, "&nbsp;&nbsp;&nbsp;&nbsp;")
+					.replace(/ /g, "&nbsp;");
+			}
+			if ((keys && keys.indexOf("compile") >= 0) || parameters.compile) {
+				var context = {};
+				Object.keys(vnode.context.$props).map(function(key) {
+					context[key] = vnode.context.$props[key];
+				});
+				// data overwrites props if necessary
+				Object.keys(vnode.context.$data).map(function(key) {
+					context[key] = vnode.context.$data[key];
+				});
+				var component = Vue.extend({
+					data: function() {
+						return context;
+					},
+					template: "<div>" + (typeof(content) == "string" ? content : content.innerHTML) + "</div>" 
+				});
+				content = new component();
+				content.$mount();
+				element.appendChild(content.$el);
+			}
+			else if (typeof(content) == "string") {
+				element.innerHTML = content;
+			}
+			else {
+				element.appendChild(content);
+			}
 		}
 	}
 });
